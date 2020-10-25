33601a2f74e8795220a5698661059d4132ac94b7
hujingfeng <57652987+hujingfeng@users.noreply.github.com>
2020-10-25 15:13:43 +0800

    Add files via upload

---
+++ Solidworks_Features/FileClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solidworks_Features
+{
+    class FileClass
+    {
+        public static void TestFunction(string FilePath)
+        {
+            Console.WriteLine(FilePath);
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> myDictionary1 = new Dictionary<int, string>();
+            Dictionary<int, string> myDictionary2 = new Dictionary<int, string>();
+
+            myDictionary1.Add(4, "Arc");
+            myDictionary1.Add(11, "Cylinder");
+            myDictionary1.Add(14, "Dimension");
+            myDictionary1.Add(5, "Ellipse");
+            myDictionary1.Add(8, "Hatch");
+            myDictionary1.Add(3, "Line");
+            myDictionary1.Add(6, "Parabola");
+            myDictionary1.Add(10, "Plane");
+            myDictionary1.Add(2, "Point");
+            myDictionary1.Add(12, "Sphere");
+            myDictionary1.Add(7, "Spline");
+            myDictionary1.Add(1, "SubSketch");
+            myDictionary1.Add(13, "Surface");
+            myDictionary1.Add(9, "Text");
+            myDictionary1.Add(0, "Unknown");
+            myDictionary2.Add(0, "All");
+            myDictionary2.Add(6, "Broken");
+            myDictionary2.Add(1, "Dangling");
+            myDictionary2.Add(4, "Context");
+            myDictionary2.Add(3, "External");
+            myDictionary2.Add(5, "Locked");
+            myDictionary2.Add(2, "Defining");
+            myDictionary2.Add(7, "Entities");
+        }
+
+    }
+}
---
+++ Solidworks_Features/Form2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solidworks_Features
+{
+    public partial class Form2 : Form
+    {
+        public string path;
+        int cancel = 0;
+
+        public Form2(string f1path)
+        {
+            this.path = f1path;
+            InitializeComponent();
+            progressBar1.Maximum = 100;                                                               //进度条最大值
+            progressBar1.Value = progressBar1.Minimum = 0;                                  //进度条最小值与当前值
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread t2 = new Thread(new ParameterizedThreadStart(function));         //创建线程
+            t2.IsBackground = true;
+            t2.Start(this.path);                                                                                          //开始线程
+        }
+
+        private delegate void DeFun(int ipos);                                                         //委托，用于传参
+
+        private void SetPos(int ipos)                                                                        //委托实例化，用于调整进度条的值
+        {
+            if(this.progressBar1.InvokeRequired)
+            {
+                DeFun df = new DeFun(SetPos);
+                this.Invoke(df, new object[] { ipos });
+            }
+            else
+            {
+                this.progressBar1.Value = ipos;                                                           //更改进度条的值
+            }
+        }
+
+        private void function(object data)                                                               //线程所调用的函数
+        {
+            string file_path = data.ToString();
+            string[] files = Directory.GetFiles(file_path);
+            int num = files.Length;
+            int cou = 0;
+            
+            foreach(string fil in files)
+            {
+                if (cancel == 0)
+                {
+                    cou++;
+                    FileClass.TestFunction(fil);                                                                 //所调用的针对单个模型文件的操作函数，也就是后续仅需编写该函数便可。
+                    SetPos((int)((float)cou / (float)num * 100));
+                }
+                else
+                {
+                    Thread.CurrentThread.Abort();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cancel = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
